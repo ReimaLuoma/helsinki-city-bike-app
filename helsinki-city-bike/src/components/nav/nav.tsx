e7dc56d43569898a1fd7976574c4ac8b218bfd0b
Reima <reima.luoma@gmail.com>
2023-04-26 14:08:13 +0300

    nav.tsx component produce broken class styling to NavLink

--- helsinki-city-bike/src/components/nav/nav.tsx
+++ helsinki-city-bike/src/components/nav/nav.tsx
@@ -2,28 +2,22 @@ import importCSV from '../../assets/icons8-import-csv-100.png'
 import bikeParking from '../../assets/icons8-bike-parking-100.png'
 import journey from '../../assets/icons8-waypoint-map-100.png'
 
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
 
     return (
         <>
         <div className='col nav-container'>
-            <Link to={`/data-import`}>
-                <div className="nav-link">
+            <NavLink to='/data-import' className={({isActive}) => isActive ? "nav-link-active": "nav-link"}>
                     <img src={importCSV} alt='' />
-                </div>
-            </Link>
-            <Link to={`/stations`}>
-                <div className="nav-link">
+            </NavLink>
+            <NavLink to='/stations' className={({isActive}) => isActive ? "nav-link-active": "nav-link"}>
                     <img src={bikeParking} alt='' />
-                </div>
-            </Link>
-            <Link to={`/journeys`}>
-                <div className="nav-link">
+            </NavLink>
+            <NavLink to='/journeys' className={({isActive}) => isActive ? "nav-link-active": "nav-link"}>
                     <img src={journey} alt='' />
-                </div>
-            </Link>
+            </NavLink>
         </div>
         </>
     )
